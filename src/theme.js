089767fa035f2caf6219159bbb62fc89054b1256
Matt Maurer <mmaurer@elevatemyeyes.com>
2026-06-19 16:47:45 -0400

    Fix component filename casing for Linux build

---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const colors = {
+  blue: '#015283',      // deep blue
+  green: '#19C93E',     // bright green
+  navy: '#0B2331',      // wordmark / dark backgrounds
+  navyLight: '#16344a', // slightly lighter navy for accents
+  text: '#e8eef2',      // light text on dark
+  textMuted: '#9fb3c0', // muted light text
+};
